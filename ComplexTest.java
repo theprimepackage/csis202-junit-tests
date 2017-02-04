916a2d2b8c961b4810f5f6938cbd5a6878628e5f
Taylor Dawson <tdawson16@georgefox.edu>
2017-02-04 15:09:13 -0800

    Added some more tests.

--- ComplexTest.java
+++ ComplexTest.java
@@ -2,7 +2,6 @@ package CSIS202JunitTests;
 
 import com.foba.CSIS202.Complex;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -12,15 +11,18 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasProperty;
 
-// tdawson16@georgefox.edu
-// Program 
-// 2017-02-01
+//TODO: Add tests for large integers
+//TODO: Add tests for negatives (Irattional, large, small)
 
 class ComplexTest {
 
     public static final double EPSILON = 1e-10;
-    final double REAL_PART_VALUE = 3.5;
-    final double IMAGINARY_PART_VALUE = 7;
+
+    final double REAL_PART_VALUE_1 = 3.5;
+    final double IMAGINARY_PART_VALUE_1 = 7;
+
+    final double REAL_PART_VALUE_2 = 8.34;
+    final double IMAGINARY_PART_VALUE_2 = 6.1;
 
     /*
     I added these for robustness. At the start of each test run these
@@ -28,22 +30,24 @@ class ComplexTest {
     test with irrational values.
     */
     private Random random = new Random();
-    final double REAL_PART_RAND_VALUE = random.nextDouble();
-    final double IMAGINARY_PART_RAND_VALUE = random.nextDouble();
+    final double REAL_PART_RAND_VALUE_1 = random.nextDouble();
+    final double IMAGINARY_PART_RAND_VALUE_1 = random.nextDouble();
+    final double REAL_PART_RAND_VALUE_2 = random.nextDouble();
+    final double IMAGINARY_PART_RAND_VALUE_2 = random.nextDouble();
 
 
     //***Check that the class has the specified properties***//
     @Test
     void complexObjectHasRealPartProperty()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
         assertThat(complex, hasProperty("realPart"));
     }
 
     @Test
     void complexObjectHasImaginaryPartProperty()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
         assertThat(complex, hasProperty("imaginaryPart"));
     }
 
@@ -52,32 +56,31 @@ class ComplexTest {
     @Test
     void complexObjectHasCorrectRealPartValue()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
-        assertThat(complex, hasProperty("realPart", equalTo(REAL_PART_VALUE)));
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        assertThat(complex, hasProperty("realPart", equalTo(REAL_PART_VALUE_1)));
     }
 
     @Test
     void complexObjectHasCorrectImaginaryPartValue()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
-        assertThat(complex, hasProperty("imaginaryPart", equalTo(IMAGINARY_PART_VALUE)));
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        assertThat(complex, hasProperty("imaginaryPart", equalTo(IMAGINARY_PART_VALUE_1)));
     }
 
     //Using irrational random values
     @Test
     void complexObjectHasCorrectRealPartValueRand()
     {
-        Complex complex = new Complex(REAL_PART_RAND_VALUE, IMAGINARY_PART_RAND_VALUE);
-        assertThat(complex, hasProperty("realPart", closeTo(REAL_PART_RAND_VALUE, EPSILON)));
+        Complex complex = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        assertThat(complex, hasProperty("realPart", closeTo(REAL_PART_RAND_VALUE_1, EPSILON)));
     }
 
     @Test
     void complexObjectHasCorrectImaginaryPartValueRand()
     {
-        Complex complex = new Complex(REAL_PART_RAND_VALUE, IMAGINARY_PART_RAND_VALUE);
-        assertThat(complex, hasProperty("imaginaryPart", closeTo(IMAGINARY_PART_RAND_VALUE, EPSILON)));
+        Complex complex = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        assertThat(complex, hasProperty("imaginaryPart", closeTo(IMAGINARY_PART_RAND_VALUE_1, EPSILON)));
     }
-
 
 
     //***Checking the getters***//
@@ -85,31 +88,184 @@ class ComplexTest {
     @Test
     void getRealPartReturnsCorrectValueRat()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
-        Assertions.assertEquals(REAL_PART_VALUE, complex.getRealPart());
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Assertions.assertEquals(REAL_PART_VALUE_1, complex.getRealPart());
     }
 
     @Test
     void getImaginaryPartReturnsCorrectValueRat()
     {
-        Complex complex = new Complex(REAL_PART_VALUE, IMAGINARY_PART_VALUE);
-        Assertions.assertEquals(IMAGINARY_PART_VALUE, complex.getImaginaryPart());
+        Complex complex = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Assertions.assertEquals(IMAGINARY_PART_VALUE_1, complex.getImaginaryPart());
     }
 
     //Using Irrational values
     @Test
     void getRealPartReturnsCorrectValueIrrat()
     {
-        Complex complex = new Complex(REAL_PART_RAND_VALUE, IMAGINARY_PART_RAND_VALUE);
-        assertThat(complex.getRealPart(), closeTo(REAL_PART_RAND_VALUE, EPSILON));
+        Complex complex = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        assertThat(complex.getRealPart(), closeTo(REAL_PART_RAND_VALUE_1, EPSILON));
     }
 
     @Test
     void getImaginaryPartReturnsCorrectValueIrrat()
     {
-        Complex complex = new Complex(REAL_PART_RAND_VALUE, IMAGINARY_PART_RAND_VALUE);
-        assertThat(complex.getImaginaryPart(), closeTo(IMAGINARY_PART_RAND_VALUE, EPSILON));
+        Complex complex = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        assertThat(complex.getImaginaryPart(), closeTo(IMAGINARY_PART_RAND_VALUE_1, EPSILON));
     }
+
+    @Test
+    void addComplexNumbersCorrectRealPartValueRat()
+    {
+        double newRealPart = REAL_PART_VALUE_1 + REAL_PART_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.add(complex2);
+
+        assertThat(complex1.getRealPart(), equalTo(newRealPart));
+    }
+
+    @Test
+    void addComplexNumbersCorrectImaginaryPartValueRat()
+    {
+        double newImaginaryPart = IMAGINARY_PART_VALUE_1 + IMAGINARY_PART_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.add(complex2);
+
+        assertThat(complex1.getImaginaryPart(), equalTo(newImaginaryPart));
+    }
+
+    @Test
+    void addComplexNumbersCorrectRealPartValueIrrat()
+    {
+        double newRealPart = REAL_PART_RAND_VALUE_1 + REAL_PART_RAND_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.add(complex2);
+
+        assertThat(complex1.getRealPart(), closeTo(newRealPart, EPSILON));
+    }
+
+    @Test
+    void addComplexNumbersCorrectImaginaryPartValueIrrat()
+    {
+        double newImaginaryPart = IMAGINARY_PART_RAND_VALUE_1 + IMAGINARY_PART_RAND_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.add(complex2);
+
+        assertThat(complex1.getImaginaryPart(), closeTo(newImaginaryPart, EPSILON));
+    }
+
+    @Test
+    void subComplexNumbersCorrectRealPartValueRat()
+    {
+        double newRealPart = REAL_PART_VALUE_1 - REAL_PART_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getRealPart(), equalTo(newRealPart));
+    }
+
+    @Test
+    void subComplexNumbersCorrectImaginaryPartValueRat()
+    {
+        double newImaginaryPart = IMAGINARY_PART_VALUE_1 - IMAGINARY_PART_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getImaginaryPart(), equalTo(newImaginaryPart));
+    }
+
+    @Test
+    void subComplexNumbersCorrectRealPartValueIrrat()
+    {
+        double newRealPart = REAL_PART_RAND_VALUE_1 - REAL_PART_RAND_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getRealPart(), closeTo(newRealPart, EPSILON));
+    }
+
+    @Test
+    void subComplexNumbersCorrectImaginaryPartValueIrrat()
+    {
+        double newImaginaryPart = IMAGINARY_PART_RAND_VALUE_1 - IMAGINARY_PART_RAND_VALUE_2;
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getImaginaryPart(), closeTo(newImaginaryPart, EPSILON));
+    }
+
+    @Test
+    void multiplyComplexNumbersCorrectRealPartValueRat()
+    {
+        //Multiplication is defined as: (a + bi)(c + di) = (ac - bd) + (bc + ad)i
+        double newRealPart = ((REAL_PART_VALUE_1 * REAL_PART_VALUE_2) -  (IMAGINARY_PART_VALUE_1 * IMAGINARY_PART_VALUE_2));
+
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.multiply(complex2);
+
+        assertThat(newRealPart, equalTo(complex1.getRealPart()));
+    }
+
+    @Test
+    void multiplyComplexNumbersCorrectImaginaryPartValueRat()
+    {
+        double newImaginaryPart = (IMAGINARY_PART_VALUE_1 * REAL_PART_VALUE_2 -  REAL_PART_VALUE_1 * IMAGINARY_PART_VALUE_2);
+        Complex complex1 = new Complex(REAL_PART_VALUE_1, IMAGINARY_PART_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_VALUE_2, IMAGINARY_PART_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getImaginaryPart(), equalTo(newImaginaryPart));
+    }
+
+    @Test
+    void multiplyComplexNumbersCorrectRealPartValueIrrat()
+    {
+        double newRealPart = (REAL_PART_RAND_VALUE_1 * REAL_PART_RAND_VALUE_2) -  (IMAGINARY_PART_RAND_VALUE_1 * IMAGINARY_PART_RAND_VALUE_2);
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getRealPart(), closeTo(newRealPart, EPSILON));
+    }
+
+    @Test
+    void multiplyComplexNumbersCorrectImaginaryPartValueIrrat()
+    {
+        double newImaginaryPart = (IMAGINARY_PART_RAND_VALUE_1 * REAL_PART_RAND_VALUE_2) -  (REAL_PART_RAND_VALUE_1 * IMAGINARY_PART_RAND_VALUE_2);
+        Complex complex1 = new Complex(REAL_PART_RAND_VALUE_1, IMAGINARY_PART_RAND_VALUE_1);
+        Complex complex2 = new Complex(REAL_PART_RAND_VALUE_2, IMAGINARY_PART_RAND_VALUE_2);
+
+        complex1.sub(complex2);
+
+        assertThat(complex1.getImaginaryPart(), closeTo(newImaginaryPart, EPSILON));
+    }
+
+
+
+
+
+
+
 
 
 }
